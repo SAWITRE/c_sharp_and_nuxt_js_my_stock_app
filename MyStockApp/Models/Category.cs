d2e10661f6ed34ebc5d60bfdeb200906fd58cde7
TT <sawitree.chom@gmail.com>
2026-06-25 10:02:26 +0700

    add path db

--- MyStockApp/Models/Category.cs
+++ MyStockApp/Models/Category.cs
@@ -1,17 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyStockApi.Models
 {
+    [Table("categories", Schema = "public")]
     public class Category
     {
+        [Column("id")]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(100)]
+        [Column("name")]
         public string Name { get; set; } = string.Empty;
 
+        [Column("description")]
         public string? Description { get; set; }
 
+        [Column("createdat")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- MyStockApp/Models/Product.cs
+++ MyStockApp/Models/Product.cs
@@ -2,24 +2,30 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyStockApi.Models
-{
+{ 
+    [Table("products", Schema = "public")]
     public class Product
     {
+        [Column("id")]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(100)]
+        [Column("name")]
         public string Name { get; set; } = string.Empty;
 
+        [Column("price")]
         public decimal Price { get; set; }
 
+        [Column("stockquantity")]
         public int StockQuantity { get; set; }
 
         // Foreign Key
+        [Column("categoryid")]
         public int CategoryId { get; set; }
 
         // Navigation Property (ช่วยให้ EF Core เชื่อมตารางให้เราอัตโนมัติ)
-        [ForeignKey("CategoryId")]
+        // [ForeignKey("categoryid")]
         public Category? Category { get; set; }
     }
 }
